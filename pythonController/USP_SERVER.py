bcd3ab770742dc66c77ff218d319375aa1efa4ed
Ahmed ARIF <arif193@gmail.com>
2021-04-19 03:09:36 +0200

    add usp msg supprt

--- pythonController/USP_SERVER.py
+++ pythonController/USP_SERVER.py
@@ -31,10 +31,48 @@ class MyListener(stomp.ConnectionListener):
         print('disconnected')
         connect_and_subscribe(self.conn)
 
+class USP_MsgType:
+    GET = 1;
+    GET_RESP = 2;
+    NOTIFY = 3;
+    SET = 4;
+    SET_RESP = 5;
+    OPERATE = 6;
+    OPERATE_RESP = 7;
+    ADD = 8;
+    ADD_RESP = 9;
+    DELETE = 10;
+    DELETE_RESP = 11;
+    GET_SUPPORTED_DM = 12;
+    GET_SUPPORTED_DM_RESP = 13;
+    GET_INSTANCES = 14;
+    GET_INSTANCES_RESP = 15;
+    NOTIFY_RESP = 16;
+    GET_SUPPORTED_PROTO = 17;
+    GET_SUPPORTED_PROTO_RESP = 18;
+
+
+
 record = usp_record_1_1_pb2.Record()
 #msg.header
+msg = usp_msg_1_1_pb2.Msg()
+msg.header.msg_type = USP_MsgType.GET_SUPPORTED_DM
+msg.header.msg_id = "101"
+msg.body.request.get_supported_dm.obj_paths.extend(["Device."])
+msg.body.request.get_supported_dm.first_level_only = 0
+msg.body.request.get_supported_dm.return_commands = 0
+msg.body.request.get_supported_dm.return_events = 0
+msg.body.request.get_supported_dm.return_params = 1
+
 record.from_id = "test1"
 record.to_id = "test2"
+record.version = "1.0"
+record.payload_security = 0
+record.no_session_context.payload = msg.SerializeToString()
+
+print(text_format.MessageToString(record))
+print(text_format.MessageToString(msg))
+
 conn.set_listener('', MyListener(conn))
 connect_and_subscribe(conn)
 time.sleep(60)
